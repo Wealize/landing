9c2c540a20eb52ed4c459ff9a0c482965051e2d0
susomejias <jesusmejias.jm@gmail.com>
2020-12-09 11:16:10 +0100

    feat(landing): :rocket: add lighthouse CI

---
+++ lighthouserc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  ci: {
+    collect: {
+      /* Add configuration here */
+      startServerCommand: 'yarn run start',
+      startServerReadyPattern: 'ready on',
+      url: [
+        'http://localhost:3000',
+        'http://localhost:3000/profile',
+        'http://localhost:3000/contact'
+      ],
+      numberOfRuns: 3
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warning', { minScore: 0.75 }],
+        'categories:seo': ['warning', { minScore: 0.75 }],
+        'categories:best-practices': ['warning', { minScore: 0.75 }],
+        'categories:accessibility': ['warning', { minScore: 0.75 }]
+      }
+    },
+    upload: {
+      /* Add configuration here */
+      target: 'temporary-public-storage'
+    }
+  }
+}
