4ef80148864cfa562c513fa56d9693af02f425a7
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-09 10:43:32 +0100

    feat(landing): :rocket: add google tag manager (#4)

--- src/components/layout/Head.tsx
+++ src/components/layout/Head.tsx
@@ -9,5 +9,15 @@ export const Head = (): JSX.Element => (
       href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
       rel="stylesheet"
     />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer', '${process.env.GTM_ID}');`
+      }}
+    />
+    );
   </NextHead>
 )
