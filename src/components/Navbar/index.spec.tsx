3192de319e77a0fc46df47c41fc818d4451b6368
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-14 16:07:06 +0100

    Feature/WLZAA-44 add page indicator animation (#12)
    * feat(landing): :art: add page indicator animation
    
    * test(landing): :rocket: add mock useRouter
    
    * feat(landing): :rocket: update changelog

--- src/components/Navbar/index.spec.tsx
+++ src/components/Navbar/index.spec.tsx
@@ -3,7 +3,12 @@ import { mount } from 'enzyme'
 
 import NavBar from './index'
 
+// eslint-disable-next-line
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 describe('<NavBar />', () => {
+  useRouter.mockImplementation(() => ({
+    pathname: '/'
+  }))
   const wrapper = mount(<NavBar />)
 
   it('wrapper: should render', () => {
