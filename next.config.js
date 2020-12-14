5848b84ac9e037cad7b0d629e3fe1a73d8599cd1
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-14 12:33:22 +0100

    Feature/WLZAA-39 add PWA support (#8)
    * feat(landing): :art: add PWA support
    
    * feat(landing): :sparkles: update manifest
    
    * docs(landing): :memo: update changelog
    
    * docs(landing): :memo: update changelog
    
    * feat(landing): :memo: add improvements to support PWA
    
    * refactor(landing): :art: update icons
    
    * ci(landing): :rocket: add pwa category to lighthouse CI
    
    * ci(landing): :art: UPDATE ci
    
    * feat(landing): :art: update CI confi

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withPWA = require('next-pwa')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -10,3 +12,10 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public'
+  }
+})
