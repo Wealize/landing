c1129e8cece19859ce8ee821cbc3980c8045a2f4
susomejias <jesusmejias.jm@gmail.com>
2020-12-15 11:06:16 +0100

    ci(landing): :sparkles: restore CI config

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,13 @@ module.exports = {
       numberOfRuns: 2
     },
     assert: {
-      preset: 'lighthouse:recommended'
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.85 }],
+        'categories:seo': ['error', { minScore: 0.85 }],
+        'categories:best-practices': ['error', { minScore: 0.85 }],
+        'categories:accessibility': ['error', { minScore: 0.85 }],
+        'categories:pwa': ['error', { minScore: 0.9 }]
+      }
     },
     upload: {
       /* Add configuration here */
