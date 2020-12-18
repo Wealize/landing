6e27e94be1e0ca0e6312bb9c6697e7725c421da0
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-18 19:10:27 +0100

    Feature/WLZAA-54 add privacy and legal terms page design (#16)
    * feat(landing): :memo: add privacy and legal terms page design
    
    * docs(landing): :memo: update changelog
    
    * feat(landing): :sparkles: add improvements

---
+++ src/pages/privacy-legal.tsx
@@ -0,0 +1,242 @@
+import React from 'react'
+
+import { LayoutOptions } from '../interfaces/Page'
+import {
+  Container,
+  Content,
+  Title,
+  Description,
+  QuestionsContainer,
+  Question,
+  QuestionTitle,
+  QuestionDescription
+} from '../styles/pages/PrivacyLegal'
+
+const PrivacyLegal = (): JSX.Element => (
+  <Container>
+    <Content>
+      <Title>Privacy & legal</Title>
+      <Description>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Donec elementum
+          ligula eu sapien consequat eleifend. Donec nec dolor erat, condimentum
+          sagittis sem. Praesent porttitor porttitor risus, dapibus rutrum ipsum
+          gravida et. Integer lectus nisi, facilisis sit amet eleifend nec,
+          pharetra
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Donec elementum
+          ligula eu sapien consequat eleifend. Donec nec dolor erat, condimentum
+          sagittis sem. Praesent porttitor porttitor risus, dapibus rutrum ipsum
+          gravida et. Integer lectus nisi, facilisis sit amet eleifend nec,
+          pharetra
+        </p>
+      </Description>
+    </Content>
+    <QuestionsContainer>
+      <Question>
+        <QuestionTitle>¿Quiénes somos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Nuestra denominación: xxxxxxxxxxxxxxxxxxx Nuestro CIF / NIF:
+            xxxxxxxxxxxxxxxxxx
+            <br />
+            Nuestra actividad principal: xxxxxxxxxxxx
+            <br />
+            Nuestra dirección:xxxxxxxx
+            <br />
+            Nuestra dirección de correo electrónico de contacto: xxxxxxxxxxxxxx
+            <br />
+            Nuestra página web: xxxxxxxxxxxxxxxxxxxxx
+          </p>
+
+          <p>
+            Para su confianza y seguridad, le informamos que somos una entidad
+            inscrita en el siguiente Registro Mercantil /Registro Público:
+            xxxxxxx
+          </p>
+          <p>Estamos a su disposición, no dude en contactar con nosotros.</p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+      <Question>
+        <QuestionTitle>¿Para qué vamos a usar sus datos?</QuestionTitle>
+        <QuestionDescription>
+          <p>
+            Con carácter general, sus datos personales serán usados para poder
+            relacionarnos con usted y poder prestarle nuestros servicios.
+          </p>
+
+          <p>
+            Asimismo, también pueden ser usados para otras actividades, como
+            enviarle publicidad o promocionar nuestras actividades.{' '}
+          </p>
+        </QuestionDescription>
+      </Question>
+    </QuestionsContainer>
+  </Container>
+)
+
+export const getStaticProps = async (): Promise<{
+  props: { layoutOptions: LayoutOptions }
+}> => {
+  const layoutOptions: LayoutOptions = {
+    showFooter: true,
+    showNavigationBarClosablePage: true
+  }
+
+  return { props: { layoutOptions } }
+}
+
+export default PrivacyLegal
