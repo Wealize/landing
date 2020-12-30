04e7e63f5c4a55045bb84cf8d6035ba5722337c2
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-30 17:53:02 +0100

    Feature/WLZAA-69 add component for show description for the profile page (#23)
    * feat(landing): :art: add profile description component
    
    * fix(landing): :sparkles: add improvements
    
    * docs(landing): :memo: update changelog

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,13 +5,23 @@ import { ACCENT_COLOR } from '../theme/color'
 import WealizeInANutshell from '../components/WealizeInANutshell'
 import ThingsWeAreGoodAt from '../components/ThingsWeAreGoodAt'
 import WeDeliver from '../components/WeDeliver'
-import { Container } from '../styles/pages/Profile'
+import { Container, StartedContainer, StartedDescription } from '../styles/pages/Profile'
 import Talents from '../components/Talents'
 import ServicesAndVentures from '../components/ServicesAndVentures'
+import WStaticIcon from '../components/icons/WStaticIcon'
+import profileDescriptionData from '../data/profileDescriptionData'
 
 const Profile = (): JSX.Element => {
   return (
     <Container>
+      <StartedContainer>
+        <StartedDescription>
+          {profileDescriptionData.map((paragraph: string, index: number) =>
+            <p key={index}>{paragraph}</p>
+          )}
+        </StartedDescription>
+        <WStaticIcon />
+      </StartedContainer>
       <ServicesAndVentures/>
       <ThingsWeAreGoodAt />
       <WeDeliver />
