4080d028897fa8a58f47f2bd0fbe8dafec952bd0
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2020-12-30 17:53:02 +0100

    Feature/WLZAA-69 add component for show description for the profile page (#23)
    * feat(landing): :art: add profile description component
    
    * fix(landing): :sparkles: add improvements
    
    * docs(landing): :memo: update changelog

--- src/styles/pages/Profile.ts
+++ src/styles/pages/Profile.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-import { WHITE_COLOR } from '../../theme/color'
+import { WHITE_COLOR, LIGHT_GRAY_COLOR } from '../../theme/color'
+import { singlePx, doublePx, doubleAndHalfPx, triplePx, quadruplePx } from '../../theme/space'
+import { media } from '../../theme/media'
 
 export const Container = styled.div`
   min-height: 100vh;
@@ -8,4 +10,82 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+`
+export const StartedContainer = styled.div`
+  min-height: 50vh;
+  background: ${LIGHT_GRAY_COLOR};
+  padding: ${doubleAndHalfPx()} ${singlePx()};
+  line-height: 1.32;
+  position: relative;
+  z-index: 1;
+
+  > .w-static-logo {
+    position: absolute;
+    bottom: -15%;
+    right: -50%;
+    height: 50%;
+    z-index: -1;
+  }
+
+  ${media.greaterThan('md')`
+    padding: ${triplePx()} ${doublePx()};
+
+    > .w-static-logo {
+      bottom: -15%;
+      right: -50%;
+      width: 90%;
+      height: 50%;
+    }
+  `};
+
+  ${media.greaterThan('lg')`
+    > .w-static-logo {
+      bottom: -30%;
+      right: -50%;
+      height: 90%;
+    }
+  `}
+
+  ${media.greaterThan('xl')`
+    padding: ${quadruplePx()};
+
+    > .w-static-logo {
+      bottom: -40%;
+      right: -50%;
+      height: 85%;
+    }
+  `};
+
+  ${media.greaterThan('xxl')`
+
+    > .w-static-logo {
+      bottom: -50%;
+      right: -45%;
+      height: 115%;
+    }
+  `};
+`
+
+export const StartedDescription = styled.div`
+  font-size: 2rem;
+  height: 100%;
+  z-index: 9999;
+  text-align: left;
+
+  > p {
+    padding-bottom: ${doublePx()};
+
+    &:last-child {
+      padding-bottom: 0;
+    }
+  }
+
+  ${media.greaterThan('md')`
+    font-size: 2.5rem;
+    width: 70%;
+  `};
+
+  ${media.greaterThan('xl')`
+    font-size: 3rem;
+  `};
 `
