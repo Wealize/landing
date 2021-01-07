964810178a9e6500bee9973321de1587d9626729
susomejias <jesusmejias.jm@gmail.com>
2021-01-07 10:32:03 +0100

    fix(landing): :memo: complete data for accordion

--- src/data/toolsTechStackAccordionData.ts
+++ src/data/toolsTechStackAccordionData.ts
@@ -3,20 +3,20 @@ import { AccordionSectionData } from '../interfaces/Accordion'
 export const toolsTechStackAccordionData: AccordionSectionData[] = [
   {
     title: 'Languages & frameworks',
-    data: ['Cypress', 'Jest', 'Redux', 'Fabric', 'Hyperledger Besu', 'Quorum', 'web3', 'Ether.js',
-      'Typescript', 'Javascript', 'React.js', 'Next.js', 'Python', 'Django']
+    data: ['Cypress', 'Jest', 'Redux', 'Hyperledger Fabric', 'Hyperledger Besu', 'Quorum', 'web3', 'Ether.js',
+      'Typescript', 'Javascript', 'React.js', 'Next.js', 'Python', 'Django', 'IBM Carbon', 'And Design', 'Coverage', 'PyTest', 'Danger.js', 'Mirage']
   },
   {
     title: 'Devops, Architecture & Systems',
-    data: []
+    data: ['Docker', 'AWS DynamoDB', 'Heroku', 'AWS S3', 'AWS Lambda', 'Alastria Blockchain Network', 'GitHub Actions', 'GitHub', 'AWS Cloudformation', 'AWS EKS', 'Terraform', 'Ansible', 'Kubernetes', 'AWS EC2', 'Wordpress', 'IBM Blockchain', 'Websockets']
   },
   {
     title: 'Tools & Platforms',
-    data: []
+    data: ['Sentry', 'Slack', 'Microsoft Clarity', 'Google Tag Manager', 'Segment', 'Figma', 'Storybook', 'Notion', 'IBM Watson', 'Jira', 'Abstract', 'Twilio', 'Zeplin']
   },
   {
     title: 'Techniques',
-    data: []
+    data: ['Natural Language Processing', 'CI/CD', 'TDD', 'E2E tests', 'JAMStack', 'Scrum', 'Code reviews', 'Pair programming', 'Design Systems', 'Web', 'SOLID', 'User Story Mapping', 'Hexagonal Architecture']
   }
 ]
 
