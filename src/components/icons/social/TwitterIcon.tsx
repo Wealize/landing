1c9a08d301788fd04945d346f8346d953d76c407
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2021-01-07 10:59:37 +0100

    Fix/WLZAA-70 completed data for accordion on the profile page (#26)
    * fix(landing): :memo: complete data for accordion
    
    * fix(landing): :art: fix columns numbers on accordion component
    
    * fix(landing): :sparkles: fix linter issues

--- src/components/icons/social/TwitterIcon.tsx
+++ src/components/icons/social/TwitterIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TwitterIcon = (props: React.SVGProps<SVGSVGElement>) => {
+const TwitterIcon = (props: React.SVGProps<SVGSVGElement>): JSX.Element => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
