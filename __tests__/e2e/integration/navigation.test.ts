a0f11980b6d7939dcb566b47463c4d8466029c11
susomejias <jesusmejias.jm@gmail.com>
2021-01-11 18:49:54 +0100

    ci(landing): :sparkles: fix ci

--- __tests__/e2e/integration/navigation.test.ts
+++ __tests__/e2e/integration/navigation.test.ts
@@ -12,6 +12,7 @@ import {
 describe('User want navigate between pages', () => {
   it('Should navigate to home page', () => {
     cy.visit(HOME_HREF)
+    cy.viewport('macbook-13')
   })
 
   describe('User want see profile page', () => {
@@ -20,6 +21,8 @@ describe('User want navigate between pages', () => {
     })
 
     it('Location should be profile page', () => {
+      cy.intercept(PROFILE_HREF, []).as('getProfile')
+      cy.wait('@getProfile', { timeout: 15000 })
       cy.location('pathname').should('eq', PROFILE_HREF)
     })
   })
@@ -30,62 +33,50 @@ describe('User want navigate between pages', () => {
     })
 
     it('Location should be home page', () => {
+      cy.intercept(HOME_HREF, []).as('getHome')
+      cy.wait('@getHome', { timeout: 15000 })
       cy.location('pathname').should('eq', HOME_HREF)
     })
   })
 
   describe('User want see privacy page', () => {
     it('Should click on privacy link', () => {
-      cy.get('footer').scrollIntoView()
       cy.get('[data-cy="footer-privacy-link"]').click({ force: true })
     })
 
     it('Location should be privacy page', () => {
+      cy.intercept(PRIVACY_LEGAL_HREF, []).as('getPrivacy')
+      cy.wait('@getPrivacy', { timeout: 15000 })
       cy.location('pathname').should('eq', PRIVACY_LEGAL_HREF)
     })
   })
 
   describe('User want see CDTI page', () => {
-    before(() => {
-      cy.visit(HOME_HREF)
-    })
-
-    it('Should click on cdti link', () => {
-      cy.get('footer').scrollIntoView()
-      cy.get('[data-cy="footer-cdti-link"]').click({ force: true })
-    })
-
     it('Location should be CDTI page', () => {
+      cy.visit(HOME_HREF)
+      cy.intercept(CDTI_HREF, []).as('getCdti')
+      cy.get('[data-cy="footer-cdti-link"]').click({ force: true })
+      cy.wait('@getCdti', { timeout: 15000 })
       cy.location('pathname').should('eq', CDTI_HREF)
     })
   })
 
   describe('User want see Innovation PYME page', () => {
-    before(() => {
-      cy.visit(HOME_HREF)
-    })
-
-    it('Should click on innovation pyme link', () => {
-      cy.get('footer').scrollIntoView()
-      cy.get('[data-cy="footer-innovation-pyme-link"]').click({ force: true })
-    })
-
     it('Location should be Innovation PYME page', () => {
+      cy.visit(HOME_HREF)
+      cy.intercept(INNOVATION_SME_HREF, []).as('getInnovation')
+      cy.get('[data-cy="footer-innovation-pyme-link"]').click({ force: true })
+      cy.wait('@getInnovation', { timeout: 15000 })
       cy.location('pathname').should('eq', INNOVATION_SME_HREF)
     })
   })
 
   describe('User want see get in touch page', () => {
-    before(() => {
-      cy.visit(HOME_HREF)
-    })
-
-    it('Should click on get in touch link', () => {
-      cy.get('footer').scrollIntoView()
-      cy.get('#footer-get-in-touch-button').click({ force: true })
-    })
-
     it('Location should be get in touch page', () => {
+      cy.visit(HOME_HREF)
+      cy.intercept(GET_IN_TOUCH_HREF, []).as('getInTouch')
+      cy.get('#footer-get-in-touch-button').click({ force: true })
+      cy.wait('@getInTouch', { timeout: 15000 })
       cy.location('href').should('eq', GET_IN_TOUCH_HREF)
     })
   })
