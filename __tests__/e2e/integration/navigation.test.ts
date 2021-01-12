7ba7b24f45b37a6a570146706996e82822cb6676
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2021-01-12 09:26:15 +0100

    Feature/WLZAA-77 add e2e navigations tests (#28)
    * test(landing): :white_check_mark: add e2e navigations tests
    
    * ci(landing): :rocket: replace cypress browser
    
    * ci(landing): :sparkles: fix ci
    
    * ci(landing): :sparkles: test ci
    
    * ci(landing): :sparkles: fi CI
    
    * ci(landing): :sparkles: fix ci
    
    * test: :white_check_mark: update snap

---
+++ __tests__/e2e/integration/navigation.test.ts
@@ -0,0 +1,81 @@
+import {
+  HOME_HREF,
+  PROFILE_HREF,
+  PRIVACY_LEGAL_HREF,
+  CDTI_HREF,
+  INNOVATION_SME_HREF
+} from '../../../src/constants/hrefs'
+import {
+  GET_IN_TOUCH_HREF
+} from '../../../src/constants/contacts'
+
+describe('User want navigate between pages', () => {
+  it('Should navigate to home page', () => {
+    cy.visit(HOME_HREF)
+    cy.viewport('macbook-13')
+  })
+
+  describe('User want see profile page', () => {
+    it('Should click on profile link', () => {
+      cy.get('[data-cy=nav-profile-link]').click({ force: true })
+    })
+
+    it('Location should be profile page', () => {
+      cy.location('pathname').should('eq', PROFILE_HREF)
+    })
+  })
+
+  describe('User want see home page when click logo', () => {
+    it('Should click on logo link', () => {
+      cy.get('[data-cy="navbar-logo-container"]').click({ force: true })
+    })
+
+    it('Location should be home page', () => {
+      cy.location('pathname').should('eq', HOME_HREF)
+    })
+  })
+
+  describe('User want see privacy page', () => {
+    it('Should click on privacy link', () => {
+      cy.get('footer').scrollIntoView()
+      cy.get('[data-cy="footer-privacy-link"]').click({ force: true })
+    })
+
+    it('Location should be privacy page', () => {
+      cy.location('pathname').should('eq', PRIVACY_LEGAL_HREF)
+    })
+  })
+
+  describe('User want see CDTI page', () => {
+    it('Should click on cdti link', () => {
+      cy.get('#nav-return-button').click()
+      cy.get('[data-cy="footer-cdti-link"]').click({ force: true })
+    })
+
+    it('Location should be CDTI page', () => {
+      cy.location('pathname').should('eq', CDTI_HREF)
+    })
+  })
+
+  describe('User want see Innovation PYME page', () => {
+    it('Should click on innovation pyme link', () => {
+      cy.get('#nav-return-button').click()
+      cy.get('[data-cy="footer-innovation-pyme-link"]').click({ force: true })
+    })
+
+    it('Location should be Innovation PYME page', () => {
+      cy.location('pathname').should('eq', INNOVATION_SME_HREF)
+    })
+  })
+
+  describe('User want see get in touch page', () => {
+    it('Should click on get in touch link', () => {
+      cy.get('#nav-return-button').click()
+      cy.get('#footer-get-in-touch-button').click({ force: true })
+    })
+
+    it('Location should be get in touch page', () => {
+      cy.location('href').should('eq', GET_IN_TOUCH_HREF)
+    })
+  })
+})
