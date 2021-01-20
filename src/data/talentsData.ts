d0389149024e561565f29b401999939ac8cc9b8d
susomejias <jesusmejias.jm@gmail.com>
2021-01-20 13:50:42 +0100

    feat(landing): :memo: review talents component

--- src/data/talentsData.ts
+++ src/data/talentsData.ts
@@ -5,7 +5,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'miguel_calero',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Miguel Calero',
-    description: 'Miguel Calero is a serial entrepreneur, consultant, lecturer, speaker, innovation catalyst in spaces where business digitalization adds a potential for a relevant change, and father of two children.',
+    role: 'Co-founder',
     socialNetworks: [
       {
         name: 'Twitter',
@@ -21,7 +21,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'javier_aguirre',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Javier Aguirre',
-    description: 'Javi is a software engineer. He is obsessed with learning new ways of improving himself and stay awake in the technology world. Believes in the startup way of thinking to make an adaptable and resourceful person.',
+    role: 'Co-founder',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -37,7 +37,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'dani_luque',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Dani Luque',
-    description: 'Dani is a Software Engineer, passionate about building and delivering high-quality software. He cares about automation, team working and continuous improvement.',
+    role: 'Software Engineer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -53,7 +53,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'dani_ranchal',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Dani Ranchal',
-    description: 'Daniel is a software engineer graduate. He is interested in the world of artificial intelligence.',
+    role: 'Software Engineer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -69,7 +69,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'jesus_mejias',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Jesús Mejías',
-    description: 'Jesús is a Full-Stack Web Developer. Passionate about technology, Open-source, teamwork, always wanting to keep learning.',
+    role: 'Software Developer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -89,7 +89,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'miguel_gavilan',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Miguel A. Gavilán',
-    description: 'Miguel is a hard-working, charismatic person and a bit perfectionist. He never thinks something is good enough. He loves to feel fulfilled, so he never stops researching about new technologies.',
+    role: 'Software Developer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -105,7 +105,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'maca_torralba',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Maca Torralba',
-    description: 'Maca is a visual designer specialized in UX / UI with skills in brand and identity, lean startups, agile, communication and dealing with the client.',
+    role: 'UX/UI Designer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -121,7 +121,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'angel_heredia',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Ángel Heredia',
-    description: 'Ángel is a computer software engineer and a member of the Aula de Software Libre. Passionate about software development and video games. He is always learning new things about technology, in fields like cybersecurity, numerical analysis or AI.',
+    role: 'Software Engineer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -137,7 +137,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'antonio_romero',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Antonio Romero',
-    description: 'Antonio is a software developer, forerunner of good vibes, who enjoys programming, challenges, teamwork, coffee and music. Always willing to continue learning. The greater the effort, the greater the glory!.',
+    role: 'Software Developer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -149,7 +149,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'nieves_borrero',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Nieves Borrero',
-    description: 'Nieves Borrero is a Software Developer who enjoys programming and converting code into a piece of art that improves other people\'s lives that\'s why she also is dedicated to colaborate as Organizer in Google Developers Group and Women TechMackers.',
+    role: 'Software Developer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -165,7 +165,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'victoria_perez',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Victoria Pérez',
-    description: 'description',
+    role: 'Software Engineer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -177,7 +177,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'marisa_ruiz',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Marisa Ruiz',
-    description: 'description',
+    role: 'role',
     socialNetworks: [
     ]
   },
@@ -185,7 +185,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'joaquin_lopez',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Joaquín López',
-    description: 'description',
+    role: 'role',
     socialNetworks: [
     ]
   },
@@ -193,7 +193,7 @@ const talentsData: CardTalent[] = [
     trans_key: 'toni_parn',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Toni Pärn',
-    description: 'I am just here to help :).',
+    role: 'Business developer',
     socialNetworks: [
       {
         name: 'Linkedin',
@@ -202,34 +202,10 @@ const talentsData: CardTalent[] = [
     ]
   },
   {
-    trans_key: 'miriam_flores',
-    imageUrl: '/img/talents/placeholder.png',
-    name: 'Miriam Flores',
-    description: 'She\'s a versatile and dynamic person who seeks to excel in everything she does. She loves learning about completely different disciplines and developing herself in new areas because she’s one of those who think that all knowledge is good.',
-    socialNetworks: [
-      {
-        name: 'Linkedin',
-        url: 'https://es.linkedin.com/in/mfarribas'
-      }
-    ]
-  },
-  {
-    trans_key: 'rafael_rodriguez',
-    imageUrl: '/img/talents/placeholder.png',
-    name: 'Rafael Rodríguez',
-    description: 'description',
-    socialNetworks: [
-      {
-        name: 'Linkedin',
-        url: 'https://es.linkedin.com/in/mfarribas'
-      }
-    ]
-  },
-  {
     trans_key: 'mariano_morera',
     imageUrl: '/img/talents/placeholder.png',
     name: 'Mariano Morera',
-    description: 'Natural from Sevilla who studied in Córdoba and lived in other places like Barcelona and Madrid (also other countries). Graduated in business, worked with technology, and interested in design. .',
+    role: 'Business specialist',
     socialNetworks: [
       {
         name: 'Linkedin',
