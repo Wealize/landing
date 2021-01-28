3ac25a07e023049d8176f8a24c1ef65167045831
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2021-01-28 13:45:45 +0100

    Feature/add documentation improvements (#46)
    * docs: :memo: update docs
    
    * refactor: :recycle: remove unused code
    
    * refactor: :recycle: remove make-badges script
    
    * docs: :memo: update readme

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   preset: 'ts-jest',
+  coverageProvider: 'v8',
   collectCoverage: true,
   transform: {
     '^.+\\.tsx?$': 'babel-jest'
   },
   coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'html'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!src/components/Global.tsx',
+    '!src/components/icons/**/*.tsx',
+    '!src/components/layout/**/*.{ts,tsx}',
+    '!src/data/**/*.ts',
+    '!src/utils/testHook.tsx',
+    '!src/interfaces/**/*.ts',
+    '!src/styles/**/*.ts',
+    '!src/pages/**/*.{ts,tsx}',
+    '!__tests__/**'
+  ],
   testPathIgnorePatterns: [
     '<rootDir>/__tests__/e2e',
     '<rootDir>/__tests__/__mocks__',
