ec1889333a8d3781b46c4d17be4be8c64f4debf9
susomejias <jesusmejias.jm@gmail.com>
2021-01-29 14:39:54 +0100

    feat: :sparkles: move static files to S3

--- next.config.js
+++ next.config.js
@@ -15,14 +15,27 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-module.exports = {
-  images: {
-    deviceSizes: [480, 576, 768, 992, 1366, 1600]
-  }
-}
-
 module.exports = nextTranslate(
   withPWA({
+    images: {
+      deviceSizes: [
+        16,
+        32,
+        48,
+        64,
+        96,
+        128,
+        256,
+        384,
+        480,
+        576,
+        768,
+        992,
+        1366,
+        1600
+      ],
+      domains: ['localhost', 'wlz-landing-assets.s3.eu-central-1.amazonaws.com']
+    },
     pwa: {
       disable: process.env.NODE_ENV === 'development',
       dest: 'public',
