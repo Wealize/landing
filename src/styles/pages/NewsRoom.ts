2c508473f0e9c7ce2cb35852430f1e59688a8306
Jesús Mejías Leiva <jesusmejias.jm@gmail.com>
2021-02-04 12:33:36 +0100

    Feature/[WLZAA-124][WLZAA-127] Add alternate layout in PostCard component (#53)
    * feat(landing): :art: Add alternate layout in PostCard
    
    * ci(landing): :sparkles: check build before passing test
    
    * fix(landing): :sparkles: remove console.warn
    
    * feat(landing): :sparkles: add html-react-parser
    
    * ci(landing): :sparkles: add secrets to CI
    
    * fix(landing): :fire: fix html string param

--- src/styles/pages/NewsRoom.ts
+++ src/styles/pages/NewsRoom.ts
@@ -1,22 +1,105 @@
 import styled from 'styled-components'
 
-import { WHITE_COLOR } from '../../theme/color'
-import { singleAndHalfPx } from '../../theme/space'
+import { WHITE_COLOR, GRAY_300_COLOR } from '../../theme/color'
+import { media } from '../../theme/media'
+import { singlePx, singleAndHalfPx, doublePx } from '../../theme/space'
+import { CONTAINER_SPACING } from '../../theme/spacingGuide'
 
 
-export const Container = styled.section`
+export const Container = styled.div`
   background-color: ${WHITE_COLOR};
-  min-height: 90vh;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+  ${CONTAINER_SPACING.MOBILE}
+
+  ${media.greaterThan('md')`
+    ${CONTAINER_SPACING.TABLET}
+  `};
+
+  ${media.greaterThan('lg')`
+    ${CONTAINER_SPACING.SMALL_DESKTOP};
+  `};
+
+  ${media.greaterThan('xl')`
+    ${CONTAINER_SPACING.DESKTOP};
+  `};
+
+  ${media.greaterThan('xxl')`
+    ${CONTAINER_SPACING.WIDE};
+  `};
+`
+
+export const PageHeader = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding-bottom: ${singlePx()};
+
+  ${media.greaterThan('md')`
+    width: 70%;
+    padding-bottom: ${doublePx()};
+  `};
+
+  ${media.greaterThan('lg')`
+    width: 50%;
+  `};
+`
+
+export const PageTitle = styled.h2`
+  font-size: 1.7rem;
+  font-weight: 400;
+  padding-bottom: ${singlePx()};
+
+  ${media.greaterThan('lg')`
+    font-size: 2rem;
+  `};
+
+  ${media.greaterThan('xl')`
+    font-size: 2.5rem;
+  `};
+`
+
+export const PageDescription = styled.h3`
+  font-size: 1rem;
+  font-weight: lighter;
+  line-height: 1.3;
+  color: ${GRAY_300_COLOR};
+
+  ${media.greaterThan('lg')`
+    font-size: 1.3rem;
+  `};
+
+  ${media.greaterThan('xl')`
+    font-size: 1.5rem;
+  `};
+`
+
+export const PostsContainer = styled.section`
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
 
-export const PageTitle = styled.h2`
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: ${singleAndHalfPx()} 0;
-`
+  & .post-card {
+    margin-top: ${singleAndHalfPx()};
+  }
 
-export const PostsContainer = styled.ul``
+  ${media.greaterThan('md')`
+    display: block;
+    columns: 2;
+    column-gap: 2rem;
+    padding: ${singlePx()} 0;
+  `};
+
+   ${media.greaterThan('lg')`
+    columns: 3;
+  `};
+
+  ${media.greaterThan('xl')`
+    columns: 4;
+  `};
+`
